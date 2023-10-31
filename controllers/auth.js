0bd7f34b5cce03dde094cbb0ae4aed810c5c66e8
Pavel <87939143+PavelZavadskiy@users.noreply.github.com>
2023-10-31 16:56:20 +0200

    Deleted log

--- controllers/auth.js
+++ controllers/auth.js
@@ -1,7 +1,6 @@
 const passport = require('passport');
 
 const isAuthorized = (req, res, next) => {
-  console.log('isAuthorized');
   passport.authenticate('jwt', { session: false }, (err, user) => {
     if (!user || err) {
       return res.status(401).json({
